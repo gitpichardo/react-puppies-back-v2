5bb75607d5178cb247d3541e4a32663961a9facc
gitpichardo <aopichar@gmail.com>
2023-01-30 10:49:17 -0800

    final backend

--- controllers/puppies.js
+++ controllers/puppies.js
@@ -1,4 +1,36 @@
 import { Puppy } from '../models/puppy.js'
 
+const create = async (req, res) => {
+  try{
+    const puppy = await Puppy.create(req.body)
+    res.json(puppy)
+  }catch (err){
+    res.json(err)
+  }
+}
 
-export { }
+const index = async (req, res) =>{
+  try{
+    const puppies = await Puppy.find({}).sort({createdAt: 'desc'})
+    res.json(puppies)
+  }catch (err){
+    res.json(err)
+  }
+}
+
+const deletePuppy = async (req, res) => {
+  try{
+    const puppy = await Puppy.findByIdAndDelete(req.params.id)
+    res.json(puppy)
+  }catch (err){
+    res.json(error)
+  }
+  
+}
+
+
+export {
+  create,
+  index,
+  deletePuppy as delete
+}
